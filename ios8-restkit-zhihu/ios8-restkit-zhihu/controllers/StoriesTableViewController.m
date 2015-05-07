aea286b6c12ae03c35f80b4dd1d20244ea4353b3
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-05-07 19:04:27 +1000

    [view] add loading cell in table view

--- ios8-restkit-zhihu/ios8-restkit-zhihu/controllers/StoriesTableViewController.m
+++ ios8-restkit-zhihu/ios8-restkit-zhihu/controllers/StoriesTableViewController.m
@@ -73,51 +73,35 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.stories.count;
+    return self.stories.count + 1;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"story" forIndexPath:indexPath];
+    if (indexPath.row < self.stories.count) {
+        return [self storyCellForIndexPath: indexPath];
+    } else {
+        return [self loadingCell];
+    }
+}
 
+- (UITableViewCell *)storyCellForIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"story" forIndexPath:indexPath];
+    
     StoryListItem *item = (StoryListItem *)[self.stories objectAtIndex:[indexPath row]];
     cell.textLabel.text = item.title;
     
     return cell;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+- (UITableViewCell *)loadingCell {
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"loading"];
+    UIActivityIndicatorView *activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    activityIndicator.center = cell.center;
+    [activityIndicator startAnimating];
+    
+    [cell addSubview:activityIndicator];
+    return cell;
 }
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
 
 #pragma mark - Navigation
 
