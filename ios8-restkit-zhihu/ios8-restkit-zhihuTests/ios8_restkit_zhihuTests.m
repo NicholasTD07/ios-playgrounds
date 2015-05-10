1645945de658363668b4bc1d8562ee1bc9402f5f
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-05-10 18:42:33 +1000

    [test] test how predicate work

--- ios8-restkit-zhihu/ios8-restkit-zhihuTests/ios8_restkit_zhihuTests.m
+++ ios8-restkit-zhihu/ios8-restkit-zhihuTests/ios8_restkit_zhihuTests.m
@@ -9,32 +9,39 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
+@interface Person : NSObject
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic) NSInteger age;
+
+- (instancetype)initWithName:(NSString *)name andAge:(NSInteger)age;
+@end
+
+@implementation Person
+- (instancetype)initWithName:(NSString *)name andAge:(NSInteger)age {
+    self = [super init];
+    if (self) {
+        self.name = name;
+        self.age = age;
+    }
+    return self;
+}
+@end
+
 @interface ios8_restkit_zhihuTests : XCTestCase
 
 @end
 
 @implementation ios8_restkit_zhihuTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)test_predicate {
+    Person *nick = [[Person alloc] initWithName:@"nick" andAge:25];
+    Person *nicole = [[Person alloc] initWithName:@"nicole" andAge:23];
+    NSArray *people = [NSArray arrayWithObjects:nick, nicole, nil];
+    NSPredicate *youngerThan25 = [NSPredicate predicateWithFormat:@"age < %d", 25];
+    
+    NSArray *youngPeople = [people filteredArrayUsingPredicate:youngerThan25];
+    XCTAssertTrue([youngPeople count] == 1);
+    XCTAssertEqualObjects(@[nicole], youngPeople);
 }
 
 @end
