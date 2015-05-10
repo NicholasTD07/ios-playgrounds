8912c295b0976bb6ed3763561f76ba998160d9d0
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-05-10 18:42:08 +1000

    [playground] play with predicate

--- ios8-restkit-zhihu/ios8-restkit-zhihu/MyPlayground.playground/section-1.swift
+++ ios8-restkit-zhihu/ios8-restkit-zhihu/MyPlayground.playground/section-1.swift
@@ -13,3 +13,13 @@ dateFormatter.dateFormat = "yyyy-MM-dd"
 let dateString = dateFormatter.stringFromDate(date)
 
 let dict = NSMutableDictionary()
+
+let array = ["nick", "nicole", "alice", "bob"] as NSArray
+
+let findNicks = NSPredicate(format: "self like 'nic*'", argumentArray: [])
+
+array.filteredArrayUsingPredicate(findNicks)
+
+let predicate = NSPredicate(format: "self == 'nick'", argumentArray: [])
+
+array.filteredArrayUsingPredicate(predicate)
