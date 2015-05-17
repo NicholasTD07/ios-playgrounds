6eac1c5da454a9e2d987e7c9b21494367c432fee
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-05-17 18:34:12 +1000

    [app] freshly made with liftoff

---
+++ ios8-swift-rac/ios8-swift-rac/Controllers/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  ios8-swift-rac
+//
+//  Created by Nicholas Tian on 5/17/15
+//  Copyright (c) 2015 . All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
