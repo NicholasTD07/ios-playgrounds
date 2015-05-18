5b718c434d9a8aa05af4a804cf1663a1766b688a
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-05-18 21:16:03 +1000

    [controller] add ChangePasswordViewController

---
+++ ios8-swift-rac/ios8-swift-rac/Controllers/ChangePasswordViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ChangePasswordViewController.swift
+//  ios8-swift-rac
+//
+//  Created by Nicholas Tian on 17/05/2015.
+//  Copyright (c) 2015 nickTD. All rights reserved.
+//
+
+import UIKit
+import ReactiveCocoa
+
+class ChangePasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
