d47754a359c5418d9649cf89549e31302762ce17
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-05-18 21:15:37 +1000

    [model] add User

---
+++ ios8-swift-rac/ios8-swift-rac/Models/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  ios8-swift-rac
+//
+//  Created by Nicholas Tian on 18/05/2015.
+//  Copyright (c) 2015 nickTD. All rights reserved.
+//
+
+import UIKit
+
+struct User {
+    var password: String
+}
