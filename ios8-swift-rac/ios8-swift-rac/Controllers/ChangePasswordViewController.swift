efbd167f2c27e5249e589753a5e5132e29948295
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-05-21 23:27:12 +1000

    [C] bind controller with VM

--- ios8-swift-rac/ios8-swift-rac/Controllers/ChangePasswordViewController.swift
+++ ios8-swift-rac/ios8-swift-rac/Controllers/ChangePasswordViewController.swift
@@ -17,13 +17,22 @@ class ChangePasswordViewController: UITableViewController {
     @IBOutlet weak var currentPasswordInputLabel: UILabel!
     @IBOutlet weak var saveButton: UIBarButtonItem!
 
+    var viewModel: ChangePasswordViewModel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         currentPasswordTextField.rac_textSignal() ~> RAC(currentPasswordInputLabel, "text")
 
+        viewModel = ChangePasswordViewModel(user: User(password: "fox"))
 
+        bindWithViewModel()
+    }
 
+    func bindWithViewModel() {
+        RACObserve(viewModel.user, "password") ~> RAC(currentPasswordLabel, "text")
+        currentPasswordTextField.rac_textSignal() ~> RAC(viewModel, "currentPasswordInput")
+        newPasswordTextField.rac_textSignal() ~> RAC(viewModel, "newPasswordInput")
+        confirmPasswordTextField.rac_textSignal() ~> RAC(viewModel, "confirmPasswordInput")
     }
 }
